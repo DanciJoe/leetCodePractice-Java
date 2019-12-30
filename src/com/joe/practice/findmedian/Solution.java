b53ed59f0b69bd029cfdb8c7047cfe0bd5dc3bf1
DXJoe <1085959189@qq.com>
2019-12-30 10:31:45 +0800

    新增二分查找方法O(log(m+n))

--- src/com/joe/practice/findmedian/Solution.java
+++ src/com/joe/practice/findmedian/Solution.java
@@ -28,8 +28,40 @@ public class Solution {
      * @return
      */
     private static double findMedianSortedArrays(int[] nums1, int[] nums2){
-        //TODO 时间复杂度为O（log(m+n)）的算法
-        return 0;
+        int m = nums1.length, n = nums2.length;
+        int left = (m + n + 1) / 2;
+        int right = (m + n + 2) / 2;
+        return (getKth(nums1, 0, nums2, 0, left) + getKth(nums1, 0, nums2, 0, right)) / 2.0;
+    }
+
+    /**
+     * 在两个有序数组中找第K大的数
+     * @param nums1
+     * @param start1
+     * @param nums2
+     * @param start2
+     * @param k
+     * @return
+     */
+    private static int getKth(int[] nums1, int start1, int[] nums2, int start2, int k) {
+        //特殊情况1：数组查找起始长度大于等于数组的长度，数组已经被淘汰
+        if (start1 > nums1.length - 1) {
+            return nums2[start2 + k - 1];
+        }
+        if (start2 > nums2.length - 1) {
+            return nums1[start1 + k - 1];
+        }
+        //特殊情况2：查找第一个数，找两个数组起始位置中最小的那个
+        if (k == 1) {
+            return Math.min(nums1[start1], nums2[start2]);
+        }
+        int nums1Mid = start1 + k / 2 - 1 < nums1.length ? nums1[start1 + k / 2 - 1] : Integer.MAX_VALUE;
+        int nums2Mid = start2 + k / 2 - 1 < nums2.length ? nums2[start2 + k / 2 - 1] : Integer.MAX_VALUE;
+        if (nums1Mid < nums2Mid) {
+            return getKth(nums1, start1 + k / 2, nums2, start2, k - k / 2);
+        } else {
+            return getKth(nums1, start1, nums2, start2 + k / 2, k - k / 2);
+        }
     }
 
     /**
@@ -66,11 +98,11 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {1,3};
+        /*int[] nums1 = {1,3};
         int[] nums2 = {2};
-        System.out.println(findMedianSortedArrays1(nums1,nums2));
+        System.out.println(findMedianSortedArrays1(nums1,nums2));*/
         int[] array1 = {1, 2};
         int[] array2 = {3, 4};
-        System.out.println(findMedianSortedArrays1(array1,array2));
+        System.out.println(findMedianSortedArrays(array1,array2));
     }
 }
