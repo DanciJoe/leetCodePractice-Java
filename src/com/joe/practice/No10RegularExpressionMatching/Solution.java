714769638e66f2f63268ab9f0c80209a8e45a416
DXJoe <1085959189@qq.com>
2020-01-13 18:25:36 +0800

    正则表达式求解代码重构

--- src/com/joe/practice/No10RegularExpressionMatching/Solution.java
+++ src/com/joe/practice/No10RegularExpressionMatching/Solution.java
@@ -21,60 +21,26 @@ public class Solution {
 
     /**
      * 回溯算法
-     * @param text
-     * @param pattern
+     * @param s
+     * @param p
      * @return
      */
-    private static boolean isMatch(String text, String pattern) {
-        if (pattern.isEmpty()) {
-            return text.isEmpty();
+    private static boolean isMatch(String s, String p) {
+        if (p.isEmpty()) {
+            return s.isEmpty();
         }
         //首先匹配第一个字符，检查是否相同或者匹配串是否以“.”开始
-        boolean firstMatch = (!text.isEmpty() && (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
-        if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
-            return (isMatch(text, pattern.substring(2)) || (firstMatch && isMatch(text.substring(1), pattern)));
+        boolean firstMatch = (!s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            return (isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p)));
         } else {
-            return firstMatch && isMatch(text.substring(1), pattern.substring(1));
+            return firstMatch && isMatch(s.substring(1), p.substring(1));
         }
-    }
-
-    /**
-     * 动态规划算法
-     * @param text
-     * @param pattern
-     * @return
-     */
-    private static boolean isMatch1(String text, String pattern) {
-        if (pattern.isEmpty() || text.isEmpty()) {
-            return false;
-        }
-        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
-        dp[0][0] = true;
-        //匹配空串，偶数位只要是*就删掉两位
-        for (int i = 2; i < pattern.length(); i += 2) {
-            if (pattern.charAt(i - 1) == '*') {
-                dp[0][i] = dp[0][i - 2];
-            }
-        }
-        for (int i = 1; i <= text.length(); i++) {
-            for (int j = 1; j <= pattern.length(); j++) {
-                if (text.charAt(i - 1) == pattern.charAt(j - 1) || pattern.charAt(i - 1) == '.') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (pattern.charAt(j - 1) == '*') {
-                    if (dp[i][j - 2]) {
-                        dp[i][j] = true;
-                    } else if (text.charAt(i - 1) == pattern.charAt(j - 2) || pattern.charAt(j - 2) == '.') {
-                        dp[i][j] = dp[i - 1][j];
-                    }
-                }
-            }
-        }
-        return dp[text.length()][pattern.length()];
     }
 
     public static void main(String[] args) {
-        String text = "aab";
-        String pattern = "c*a*b";
-        System.out.println(isMatch1(text,pattern));
+        String text = "ab";
+        String pattern = ".*";
+        System.out.println(isMatch(text,pattern));
     }
 }
