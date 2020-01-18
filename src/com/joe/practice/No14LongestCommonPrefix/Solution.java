4543b0bf56e95ef3cf933300098284caaee499f6
DXJoe <1085959189@qq.com>
2020-01-18 15:38:32 +0800

    查找公共前缀待解

---
+++ src/com/joe/practice/No14LongestCommonPrefix/Solution.java
@@ -0,0 +1,28 @@
+package com.joe.practice.No14LongestCommonPrefix;
+
+/**
+ * @author : Joe
+ * created : 15:16 2020/1/18
+ * description :查找字符串数组最长公共前缀
+ */
+public class Solution {
+
+    /**
+     * Write a function to find the longest common prefix string amongst an array of strings.
+     *
+     * If there is no common prefix, return an empty string "".
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/longest-common-prefix
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param strings
+     * @return
+     */
+
+    private static String longestCommonPrefix(String[] strings) {
+        for (int i = 0; i < strings.length; i++) {
+
+        }
+        return "";
+    }
+}
